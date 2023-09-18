479a8c71c5406019ffd73528cedbeb7bd47e56ce
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-18 10:15:05 -0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 
 import HomePage from "./pages/HomePage";
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './footer.css'
+const Footer = () => {
+    return (
+    <footer className="footer">
+        Copyright &#169; 2023 Rachel Fingerhut. All right received.
+    </footer>
+    );
+}
+
+export default Footer;
