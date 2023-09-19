d0f3f6e1e0a314edf07d88dcaa33d52b820488e0
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-19 11:35:33 -0700

    Add mobile responsive styles

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,6 +3,7 @@ import websiteIcon from '../../assets/websiteIcon.png';
 import fullStackIcon from '../../assets/fullStackIcon.png';
 import componentIcon from '../../assets/componentIcon.png';
 import { useNavigate } from 'react-router-dom';
+import fullStack from '../../assets/fullStack.png';
 
 const Skills = () => {
 
@@ -20,11 +21,10 @@ const Skills = () => {
                     <div className="skill-card-text">
                         <h2>Websites and Landing Pages</h2>
                         <p>For your online presence, I design and build visually appealing websites and landing pages. I use CSS, HTML, and JavaScript to ensure your site not only looks great but functions seamlessly, providing an excellent user experience.</p>
-
                     </div>
                 </div>
                 <div className="skill-card">
-                    <img src={fullStackIcon} alt="full stack web app icon" className="skill-card-img"/>
+                    <img src={fullStack} alt="full stack web app icon" className="skill-card-img" id="full-stack-icon"/>
                     <div className="skill-card-text">
                         <h2>Full Stack Web Apps</h2>
                         <p>When you need interactive and feature-rich web applications, I use React and Node.js to create dynamic experiences. For robust data management, I integrate MongoDB as the database, hosted on the Google Cloud Platform for scalability.</p>
